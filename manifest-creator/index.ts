c9ccebf34da9b89cd59bf5944dfd1dc4a3d20825
daylight <starcrush@monemail.com>
2024-10-17 14:16:23 +0300

    Reformat

--- manifest-creator/index.ts
+++ manifest-creator/index.ts
@@ -3,16 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 type Platform = 'linux' | 'windows' | 'macos';
-type PlatformVersion =
-    | '18.04'
-    | '20.04'
-    | '22.04'
-    | '2019'
-    | '2022'
-    | '10.15'
-    | '11'
-    | '12'
-    | '13';
+type PlatformVersion = '18.04' | '20.04' | '22.04' | '2019' | '2022' | '10.15' | '11' | '12' | '13';
 type Toolset = 'gcc' | 'msvc' | 'mingw' | 'clang';
 type Arch = 'x86' | 'aarch64';
 type Link = 'static' | 'shared' | 'static+shared';
@@ -31,15 +22,16 @@ const ignoredFileNames: string[] = [
     'boost-1.74.0-windows-2019-no-python.tar.gz',
 ];
 
-const notIgnoredFile = (file: File) =>
-    !ignoredFileNames.includes(file.filename);
+const notIgnoredFile = (file: File) => !ignoredFileNames.includes(file.filename);
 
 const compareTags = (a: string, b: string): 0 | 1 | -1 => {
-    var pa = a.split('.');
-    var pb = b.split('.');
-    for (var i = 0; i < 3; i++) {
-        var na = Number(pa[i]);
-        var nb = Number(pb[i]);
+    const pa = a.split('.');
+    const pb = b.split('.');
+
+    for (let i = 0; i < 3; i++) {
+        const na = Number(pa[i]);
+        const nb = Number(pb[i]);
+
         if (na > nb) return 1;
         if (nb > na) return -1;
         if (!isNaN(na) && isNaN(nb)) return 1;
@@ -50,6 +42,7 @@ const compareTags = (a: string, b: string): 0 | 1 | -1 => {
 
 async function main(): Promise<void> {
     const octokit = new Octokit();
+
     const releases = await octokit.request(
         'GET /repos/{owner}/{repo}/releases',
         {
@@ -84,18 +77,11 @@ async function main(): Promise<void> {
 
                     return {
                         filename: d.name,
-                        platform: split[2].replace(
-                            'ubuntu',
-                            'linux'
-                        ) as Platform,
+                        platform: split[2].replace('ubuntu', 'linux') as Platform,
                         platform_version: split[3] as PlatformVersion,
                         toolset: split[4] as Toolset,
-                        link:
-                            (split.length >= 6 && (split[5] as Link)) ||
-                            undefined,
-                        arch:
-                            (split.length >= 7 && (split[6] as Arch)) ||
-                            undefined,
+                        link: (split.length >= 6 && (split[5] as Link)) || undefined,
+                        arch: (split.length >= 7 && (split[6] as Arch)) || undefined,
                         download_url: d.browser_download_url,
                     };
                 })
@@ -109,6 +95,7 @@ async function main(): Promise<void> {
     );
 
     const outFile = path.join(__dirname, '..', 'versions-manifest.json');
+
     if (fs.existsSync(outFile)) {
         console.log('versions-manifest.json already exists, deleting it');
         fs.unlinkSync(outFile);
